e074902dcb5ea9a5865a92badd6eedbd29490cb0
Денис Свашенко <dsvash@yandex.ru>
2022-09-04 01:41:02 +0300

    Step: Deploy Fourth

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('У меня получилось!')
 
-# Тут вторая страница
+
 def second_page(request):
     return HttpResponse('А это вторая страница!')
